742586230fbfa53ed613fbd4cb44f49daf458bbb
chinasa05 <58641124+chinasa05@users.noreply.github.com>
2021-04-11 05:23:43 -0400

    Create play.js

---
+++ play.js
@@ -0,0 +1,17 @@
+function goHome(){
+ window.location.href = 'index.html';
+}
+
+function bat(){
+  alert("Good Job!");
+   document.getElementById("battery").style.visibility = "hidden";  
+
+}
+function disa(){ 
+    alert("Well Done!");
+     document.getElementById("apple").style.visibility = "hidden"; 
+ }
+
+function ban(){   alert("Nice Try! Refresh to try Again");
+ document.getElementById("banana").style.visibility = "hidden"; 
+ }
