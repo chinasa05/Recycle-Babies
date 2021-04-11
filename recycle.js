eccfa83d8f068759c4d5f9f323029f5314755d91
chinasa05 <58641124+chinasa05@users.noreply.github.com>
2021-04-11 05:24:06 -0400

    Create recycle.js

---
+++ recycle.js
@@ -0,0 +1,12 @@
+//this is a function to go back to the main/home page
+function goHome(){
+ window.location.href = 'index.html';
+}
+//function to go back to tutorial page
+function tutorial(){
+   window.location.href = 'tutorial.html';
+}
+//function to go to compost
+function compost(){
+   window.location.href = 'compost.html';
+}
