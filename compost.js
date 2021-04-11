bd961516e5e2c6df28c1961c98b02eda463a4d57
chinasa05 <58641124+chinasa05@users.noreply.github.com>
2021-04-11 05:23:25 -0400

    Create compost.js

---
+++ compost.js
@@ -0,0 +1,11 @@
+function goHome(){
+ window.location.href = 'index.html';
+}
+
+
+function recycle(){
+ window.location.href = 'recycle.html';
+}
+function trash(){
+ window.location.href = "trash.html";
+}
